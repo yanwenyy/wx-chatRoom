325e5530328734220329681faee99ff860ccf06a
yanwenyy <18738390058@163.com>
2020-05-14 16:57:07 +0800

    Initial Commit

---
+++ plugin/pages/hello-page.js
@@ -0,0 +1,55 @@
+// plugin/pages/hello-page.js
+Page({
+  data: {
+    list: [], //会话内容
+    inputMsg: '',
+    scrollTop: '',
+    windowHeight:'',
+    inputHeight:''
+  },
+  onLoad: function () {
+    console.log('This is a plugin page!');
+    this.setData({
+      windowHeight: wx.getSystemInfoSync().windowHeight
+    })
+  },
+
+  //input框输入
+  input: function (e) {
+    this.setData({
+      inputMsg: e.detail.value
+    })
+  },
+
+  //发送按钮点击
+  sendMsg: function () {
+    // let  scrollHeight = wx.getSystemInfoSync().windowHeight;
+    if (this.data.inputMsg != '') {
+      this.data.list.push(this.data.inputMsg);
+      this.setData({
+        inputMsg: '',
+        list: this.data.list,
+        scrollTop: this.data.list.length * 1000
+      })
+    } else {
+
+    }
+  },
+
+  inputFocus(e) {
+    console.log('键盘弹起')
+    var inputHeight = 0
+    if (e.detail.height) {
+      inputHeight = e.detail.height;
+      this.setData({
+        inputHeight:inputHeight
+      })
+    }
+  },
+  inputBlur() {
+    console.log('键盘收起');
+    this.setData({
+      inputHeight:0
+    })
+  },
+});
